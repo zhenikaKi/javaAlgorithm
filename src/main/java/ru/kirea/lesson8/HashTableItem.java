3fd9b8db9a5e53fcfe8f2af5a466a622ec3d1e9d
Кирюшин Евгений Андреевич <ZhenikaKi@gmail.com>
2021-03-13 15:53:47 +0500

    ДЗ

---
+++ src/main/java/ru/kirea/lesson8/HashTableItem.java
@@ -0,0 +1,13 @@
+package ru.kirea.lesson8;
+
+public class HashTableItem {
+    private int key;
+
+    public HashTableItem(int key) {
+        this.key = key;
+    }
+
+    public int getKey() {
+        return key;
+    }
+}
